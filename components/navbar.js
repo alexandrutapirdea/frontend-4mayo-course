d390c1f8f2b3d9d3c2c35783924aa0b706e64506
Alexandru Tapirdea <tapirdea.alexandru@yahoo.com>
2022-09-14 18:37:31 +0300

    update website

--- components/navbar.js
+++ components/navbar.js
@@ -9,7 +9,7 @@ class Navbar extends HTMLElement {
     <div class="row" class="collapse navbar-collapse">
         <div class="col-sm-3">
             <div class="logo">
-                <a href="index.html"><img src="./css/images/logo.png" alt="LaboratorTW" />
+                <a href="../index.html"><img src="./css/images/logo.png" alt="LaboratorTW" />
                 </a>
             </div>
         </div>
@@ -22,33 +22,30 @@ class Navbar extends HTMLElement {
         <div class="col-sm-9" id="navbarSupportedContent">
             <nav id="desktop-menu">
                 <ul class="sf-menu" id="navigation">
-                    <li><a href="index.html">Acasa</a>
-                    <li><a href="pages/Notare.html">Notare</a>
+                    <li><a href="../index.html">Acasa</a>
+                    <li><a href="./Notare.html">Notare</a>
                     </li>
                     <li class="nav-link disabled"><a href="#">Laboratoare <i class="fa fa-sort-amount-asc"
                                 aria-hidden="true"></i></a>
                         <ul class="dropdown-ul">
-                            <li><a href="pages/Laborator1.html">Laborator 1</a></li>
-                            <li><a href="pages/Laborator2.html">Laborator 2</a></li>
-                            <li><a href="pages/laborator3.html">Laborator 3</a></li>
-                            <li><a href="pages/Laborator4.html">Laborator 4</a></li>
-                            <li><a href="pages/Laborator5.html">Laborator 5</a></li>
-                            <li><a href="pages/Laborator6.html">Laborator 6</a></li>
-                            <li><a href="pages/Laborator7.html">Laborator 7</a></li>
-                            <li><a href="pages/Laborator8.html">Laborator 8</a></li>
-                            <li><a href="pages/Laborator9.html">Laborator 9</a></li>
-                            <li><a href="./pages/Cod_laboratoare/Laborator%2010/validariFormulare.html">Laborator
+                            <li><a href="./Laborator1.html">Laborator 1</a></li>
+                            <li><a href="./Laborator2.html">Laborator 2</a></li>
+                            <li><a href="./laborator3.html">Laborator 3</a></li>
+                            <li><a href="./Laborator4.html">Laborator 4</a></li>
+                            <li><a href="./Laborator5.html">Laborator 5</a></li>
+                            <li><a href="./Laborator6.html">Laborator 6</a></li>
+                            <li><a href="./Laborator7.html">Laborator 7</a></li>
+                            <li><a href="./Laborator8.html">Laborator 8</a></li>
+                            <li><a href="./Laborator9.html">Laborator 9</a></li>
+                            <li><a href="./Cod_laboratoare/Laborator%2010/validariFormulare.html">Laborator
                                     10_RECAPITULARE</a></li>
-                            <li><a href="./pages/Laborator10_NodeJS.html">Laborator
+                            <li><a href="./Laborator10_NodeJS.html">Laborator
                                     10_Node</a></li>
                         </ul>
 
                     </li>
-                    <li><a href="./pages/ProiectHML.html">Proiect HTML + CSS</a></li>
-                    <li><a href="./pages/ProiectJS.html">Proiect Javascript</a></li>
-                    <li class="observatie"><a href="pages/restanta.html" class="observatie"
-                            style="color:red; font-weight: bold;">Restanta</a></li>
-                    </li>
+                    <li><a href="./ProiectHML.html">Proiect HTML + CSS</a></li>
+                    <li><a href="./ProiectJS.html">Proiect Javascript</a></li>
                 </ul>
             </nav>
         </div>
--- pages/pages/Cod_laboratoare/Laborator 10/validariFormulare.js
+++
@@ -1,9 +0,0 @@
-// var pauza = document.createElement("div");
-// pauza.style.width ="100%";
-// pauza.style.height = "100%";
-// pauza.style.backgroundColor = "black";
-// pauza.style.position ="absolute";
-// pauza.style.top = 0;
-// pauza.style.left = 0;
-// pauza.style.opacity = 0.6;
-// document.body.appendChild(pauza);
